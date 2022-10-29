ed46a59930babba9cb5f8737afb39c901e588565
Federico Blancato <federico.blancato@infinitaslearning.com>
2022-10-29 21:16:50 +0200

    Use in memory index

--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,103 @@
-use std::collections::HashMap;
-use std::fs::OpenOptions;
-use std::hash::Hash;
-use std::io::{prelude::*, BufReader};
-use std::net::{TcpListener, TcpStream};
-use std::sync::Arc;
-use std::thread;
+use std::io::{Result, SeekFrom};
+use std::{collections::HashMap, sync::Arc};
+use tokio::fs::File;
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncSeekExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+type Index = HashMap<usize, (usize, usize)>;
 
-struct IndexRecord {
-    key: usize,
-    offset: usize,
-}
-type Index = HashMap<usize, usize>;
-
-fn build_index() -> Index {
-    let fd = OpenOptions::new()
-        .read(true)
-        .open("append.db")
-        .expect("could not open file");
-    let lines = BufReader::new(fd).lines();
+async fn build_index_from_db() -> Result<Index> {
+    let file = File::open("append.db").await?;
+    let mut lines = BufReader::new(file).lines();
     let mut index = HashMap::new();
     let mut index_pointer = 0;
-    for line in lines {
-        let line = line.unwrap();
+    let mut line_number = 1;
+    while let Some(line) = lines.next_line().await? {
         if line.is_empty() {
             continue;
         };
         let tokens = line.split(',');
         let tokens_vec = tokens.collect::<Vec<&str>>();
-        index.insert(tokens_vec[0].parse().unwrap(), index_pointer);
-        println!("{}, {}", tokens_vec[0], index_pointer);
+        index.insert(
+            tokens_vec[0].parse().unwrap(),
+            (
+                index_pointer + line_number,
+                tokens_vec[1].len() + tokens_vec[0].len() + 2,
+            ),
+        );
+        line_number += 1;
         index_pointer += line.len();
     }
-    index
+    Ok(index)
 }
 
-fn append_to_db(key: usize, data: &str) -> std::io::Result<usize> {
-    let mut fd = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open("append.db")
-        .expect("could not open file");
-    fd.write(format!("{},{}\n", key, data).as_bytes())
+async fn append_to_db(key: usize, data: &str) -> Result<usize> {
+    let mut file = File::open("append.db").await?;
+    file.write(format!("{},{}\n", key, data).as_bytes()).await
 }
 
-fn get_from_db(key: usize, index: &Index) -> Option<String> {
-    let index_pointer = index.get(&key)?;
-    let fd = OpenOptions::new()
-        .read(true)
-        .open("append.db")
-        .expect("could not open file");
-    let lines = BufReader::new(fd).lines();
-
-    lines
-        .filter_map(|x| x.ok())
-        .find(|line| line.starts_with(&key.to_string()))
+async fn get_from_db(key: usize, index: &Index) -> Result<String> {
+    let mut file = File::open("append.db").await?;
+    if let Some((index_pointer, size)) = index.get(&key) {
+        let mut value_from_db = vec![0u8; *size];
+        file.seek(SeekFrom::Start(*index_pointer as u64)).await?;
+        file.read_exact(&mut value_from_db).await?;
+        if let Ok(value_from_db) = std::str::from_utf8(&value_from_db) {
+            Ok(String::from(value_from_db))
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to convert utf value",
+            ))
+        }
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Key not found",
+        ))
+    }
 }
 
-fn handle_connection(mut stream: TcpStream, index: &Arc<Index>) {
-    let mut write_stream = stream.try_clone().unwrap();
-    let mut buf_reader = BufReader::new(&mut stream);
+async fn handle_connection(mut stream: TcpStream, index: &Arc<Index>) -> Result<()> {
+    let (rx, mut wx) = stream.split();
+    let mut buf_reader = BufReader::new(rx);
     let mut string_buffer = String::new();
     loop {
-        let line = buf_reader.read_line(&mut string_buffer);
+        buf_reader.read_line(&mut string_buffer).await?;
+        if string_buffer.len() == 0 {
+            continue;
+        }
         let string = string_buffer.strip_suffix("\n").unwrap();
 
         let tokens: Vec<&str> = string.split(' ').collect();
-        match line {
-            Ok(0) => break,
-            Ok(_) => match tokens[0] {
-                "get" => {
-                    let key = tokens[1].parse::<usize>().expect("key should be a number");
-                    let record = get_from_db(key, index).expect("key not found");
-                    write_stream
-                        .write_all(record.as_bytes())
-                        .expect("failed to write to stream");
-                    write_stream
-                        .write_all(b"\n")
-                        .expect("failed to write to stream");
-                }
-                "set" => {
-                    let key = tokens[1].parse::<usize>().expect("key should be a number");
-                    append_to_db(key, &tokens[2..].join(" ")).expect("could not write to db");
-                    write_stream
-                        .write_all(b"Ok")
-                        .expect("failed to write to stream");
-                    write_stream
-                        .write_all(b"\n")
-                        .expect("failed to write to stream");
-                }
-                _ => println!("only supported commands are get/set"),
-            },
-            Err(e) => {
-                println!("Error {e}");
-                break;
+        match tokens[0] {
+            "get" => {
+                let key = tokens[1].parse::<usize>().expect("key should be a number");
+                let record = get_from_db(key, index).await?;
+                wx.write_all(record.as_bytes()).await?;
+                wx.write_all(b"\n").await?;
             }
+            "set" => {
+                let key = tokens[1].parse::<usize>().expect("key should be a number");
+                append_to_db(key, &tokens[2..].join(" ")).await?;
+                wx.write_all(b"Ok").await?;
+                wx.write_all(b"\n").await?;
+            }
+            _ => println!("only supported commands are get/set"),
         }
         string_buffer.truncate(0);
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9999").unwrap();
-    let index = build_index();
-    let arc = Arc::new(index);
-    for stream in listener.incoming() {
-        let index_clone = Arc::clone(&arc);
-        thread::spawn(move || match stream {
-            Ok(stream) => {
-                handle_connection(stream, &index_clone);
-            }
-            Err(e) => {
-                panic!("Error connecting {}", e)
-            }
-        });
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let index = build_index_from_db().await?;
+    println!("1 {:?}", index[&1]);
+    println!("2 {:?}", index[&2]);
+    let index = Arc::new(index);
+    let listener = TcpListener::bind("127.0.0.1:9999").await?;
+    loop {
+        let (socket, _) = listener.accept().await?;
+        let index = index.clone();
+        tokio::spawn(async move { handle_connection(socket, &index).await });
     }
 }
